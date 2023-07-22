c2823d181cba2050143bffd1b7934a6039519b0d
emandayrit <adridayrit@gmail.com>
2023-07-22 06:44:11 -0800

    Updated the UI
    -Replaced the UI Canvas into UIDocument
    -Added Math for clicking, upgrades, and autoclicking
    -Added a click SFX

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,175 +1,223 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Main Button")]
-    [SerializeField] int pointsValue = 0;
-    [SerializeField] TMP_Text pointsValueText;
+    #region for safe keeping
+    //private void Awake()
+    //{
+    //    SetPointText();
+    //    autoIncrementPanel.SetActive(false);
+    //}
 
-    [Header("Incremental Upgrade")]
-    public int pointsIncrement = 1;
-    public int incrementIncrease = 1;
-    public float incrementMultiplier = 1.10f;
-    [SerializeField] TMP_Text pointsIncrementText;
-    public int incrementUpgradeLevel = 1;
-    [SerializeField] TMP_Text pointsIncrementLevelText;
-    public int incrementUpgradeCost = 4;
-    [SerializeField] TMP_Text pointsIncrementCostText;
+    //private void Start()
+    //{
+    //    StartCoroutine(AutoIncrementUpdateCoroutine());
+    //}
 
-    [Header("Auto-Increment")]
-    public int autoIncrementRequiredValue = 2;
-    [SerializeField] GameObject autoIncrementPanel;
-    public int autoIncrementCost = 2;
-    [SerializeField] TMP_Text autoIncrementCostText;
-    public int autoIncrementValue = 1;
-    [SerializeField] TMP_Text autoIncrementValueText;
-    public int autoIncrementLevel = 0;
-    [SerializeField] TMP_Text autoIncrementLevelText;
+    //public void OnButtonClick()
+    //{
+    //    pointsValue += pointsIncrement;
+    //    pointsValueText.text = $"Button Points: {pointsValue}";
 
-    [Header("Auto-Increment Speed")]
-    public int autoIncrementSpeedRequiredValue = 2;
-    [SerializeField] GameObject autoIncrementSpeedPanel;
-    public int autoIncrementSpeedCost = 5;
-    public float autoIncrementinSecond = 1f;
-    [SerializeField] TMP_Text autoIncrementSpeedText;
-    [SerializeField] TMP_Text autoIncrementSpeedCostText;
+    //    if (pointsValue >= autoIncrementRequiredValue)
+    //    {
+    //        autoIncrementPanel.SetActive(true);
+    //    }
+    //}
+
+    //public void OnUpgradeClick()
+    //{
+    //    if (pointsValue >= incrementUpgradeCost)
+    //    {
+    //        pointsValue -= incrementUpgradeCost;
+    //        pointsValueText.text = $"Button Points: {pointsValue}";
+
+    //        pointsIncrement += incrementIncrease;
+    //        incrementIncrease *= Mathf.RoundToInt(incrementMultiplier);
+    //        pointsIncrementText.text = $"Click = {pointsIncrement}";
+
+    //        ++incrementUpgradeLevel;
+    //        pointsIncrementLevelText.text = $"Click Me! Level: {incrementUpgradeLevel}";
+
+    //        incrementUpgradeCost += incrementUpgradeLevel * Mathf.RoundToInt(incrementMultiplier * 2.8f);
+    //        pointsIncrementCostText.text = $"Upgrade Cost: {incrementUpgradeCost}";
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Not enough money.");
+    //    }
+    //}
+
+    //public void OnAutoUpgradeClick()
+    //{
+    //    if (pointsValue >= autoIncrementCost)
+    //    {
+    //        pointsValue -= autoIncrementCost;
+    //        pointsValueText.text = $"Button Points: {pointsValue}";
+
+    //        ++autoIncrementLevel;
+    //        autoIncrementLevelText.text = $"Auto-Click Level: {autoIncrementLevel}";
+
+    //        autoIncrementValue += (autoIncrementCost / 2);
+    //        autoIncrementValueText.text = $"Auto-Click per Second: {autoIncrementValue}";
+
+    //        autoIncrementCost *= Mathf.RoundToInt(1.5f);
+    //        autoIncrementCostText.text = $"Auto-Upgrade Cost: {autoIncrementCost}";
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Not enough money for auto increment.");
+    //    }
+
+    //    if(autoIncrementLevel >= autoIncrementSpeedRequiredValue)
+    //    {
+    //        autoIncrementSpeedPanel.SetActive(true);
+    //    }
+    //}
+
+    //public void OnUpgradeAutoIncrementSpeed()
+    //{
+    //    if(autoIncrementLevel < autoIncrementSpeedRequiredValue)
+    //    {
+    //        return;
+    //    }
+
+    //    if (pointsValue >= autoIncrementSpeedCost)
+    //    {
+    //        if(autoIncrementinSecond >= 0.01f)
+    //        {
+    //            pointsValue -= autoIncrementSpeedCost;
+    //            float _value = 1;
+    //            autoIncrementSpeedCost *= Mathf.RoundToInt(_value + autoIncrementinSecond);
+    //            autoIncrementSpeedCostText.text = $"Auto Increment Speed Cost: {autoIncrementSpeedCost}";
+
+    //            autoIncrementinSecond -= 0.1f;
+    //            if(autoIncrementinSecond <= 0.01f)
+    //            {
+    //                autoIncrementinSecond = 0.01f;
+    //                Debug.Log("You've reached the max speed!");
+    //            }
+
+    //            autoIncrementSpeedText.text = $"Auto Increment Speed: {Math.Round(autoIncrementinSecond, 2)} second/s";
+    //        }
+
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Not enough money for auto increment speed");
+    //    }
+    //}
+
+    //IEnumerator AutoIncrementUpdateCoroutine()
+    //{
+    //    yield return new WaitForSeconds(autoIncrementinSecond);
+
+    //    if(autoIncrementLevel >= 1)
+    //    {
+    //        pointsValue += autoIncrementValue;
+    //        pointsValueText.text = $"Button Points: {pointsValue}";
+
+    //    }
+
+    //    StartCoroutine(AutoIncrementUpdateCoroutine());
+    //}
+
+    //private void SetPointText()
+    //{
+    //    pointsValueText.text = $"Button Points: {pointsValue}";
+    //    pointsIncrementText.text = $"Click = {pointsIncrement}";
+    //    pointsIncrementCostText.text = $"Upgrade Cost: {incrementUpgradeCost}";
+    //    pointsIncrementLevelText.text = $"Click Me! Level: {incrementUpgradeLevel}";
+
+    //    autoIncrementLevelText.text = $"Auto-Click Level: {autoIncrementLevel}";
+    //    autoIncrementValueText.text = $"Auto-Click per Second: {autoIncrementValue}";
+    //    autoIncrementCostText.text = $"Auto-Upgrade Cost: {autoIncrementCost}";
+
+    //    autoIncrementSpeedCostText.text = $"Auto Increment Speed Cost: {autoIncrementSpeedCost}";
+    //    autoIncrementSpeedText.text = $"Auto Increment Speed: {Math.Round(autoIncrementinSecond, 2)} second/s";
+    //}
+    #endregion
+
+    public static GameManager manager;
+
+    public double _clickCounter = 0;
+    public double _clickPoints = 1;
+    public double _clickAutoPoints = 0;
+    public double _upgradeLevel = 0;
+    public double _upgradeCost = 4;
+    public double _clickAutoLevel = 0;
+    public double _clickAutoCost = 1;
+
+    public bool isAutoClickEnable = false;
+
+    private double _upgradeBaseCost = 4;
+    private double _upgradeCostMultiplier = 1.15f;
+    private double _clickAutoMultiplier = 1.53f;
+    private double _clickMultiplier = 2;
 
     private void Awake()
     {
-        SetPointText();
-        autoIncrementPanel.SetActive(false);
-    }
-
-    private void Start()
-    {
-        StartCoroutine(AutoIncrementUpdateCoroutine());
-    }
-
-    public void OnButtonClick()
-    {
-        pointsValue += pointsIncrement;
-        pointsValueText.text = $"Button Points: {pointsValue}";
-
-        if (pointsValue >= autoIncrementRequiredValue)
+        if (manager != null && manager != this)
         {
-            autoIncrementPanel.SetActive(true);
-        }
-    }
-
-    public void OnUpgradeClick()
-    {
-        if (pointsValue >= incrementUpgradeCost)
-        {
-            pointsValue -= incrementUpgradeCost;
-            pointsValueText.text = $"Button Points: {pointsValue}";
-
-            pointsIncrement += incrementIncrease;
-            incrementIncrease *= Mathf.RoundToInt(incrementMultiplier);
-            pointsIncrementText.text = $"Click = {pointsIncrement}";
-
-            ++incrementUpgradeLevel;
-            pointsIncrementLevelText.text = $"Click Me! Level: {incrementUpgradeLevel}";
-
-            incrementUpgradeCost += incrementUpgradeLevel * Mathf.RoundToInt(incrementMultiplier * 2.8f);
-            pointsIncrementCostText.text = $"Upgrade Cost: {incrementUpgradeCost}";
+            Destroy(gameObject);
         }
         else
         {
-            Debug.Log("Not enough money.");
+            manager = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
 
-    public void OnAutoUpgradeClick()
+    public void SetNextUpgrade()
     {
-        if (pointsValue >= autoIncrementCost)
+        _clickCounter -= _upgradeCost;
+        _clickCounter = Math.Round(_clickCounter, 2);
+
+        _upgradeLevel++;
+        _upgradeCost = _upgradeBaseCost * Math.Pow(_upgradeCostMultiplier, _upgradeLevel);
+        _upgradeCost = Math.Round(_upgradeCost, 2);
+
+        _clickPoints = _clickMultiplier * _upgradeLevel;
+        _clickPoints = Math.Round(_clickPoints, 2);
+    }
+
+    public void SetNextAutoClick()
+    {
+        _clickCounter -= _clickAutoCost;
+        _clickCounter = Math.Round(_clickCounter, 2);
+
+        _clickAutoLevel++;
+        _clickAutoCost = _upgradeBaseCost * Math.Pow(_upgradeCostMultiplier+0.75f, _clickAutoLevel);
+        _clickAutoCost = Math.Round(_clickAutoCost, 2);
+
+        _clickAutoPoints = _clickAutoMultiplier * _clickAutoLevel;
+        _clickAutoPoints = Math.Round(_clickAutoPoints, 2);
+
+        isAutoClickEnable = true;
+    }
+
+    public string FormattedNumber(double _value)
+    {
+        double absNumber = Math.Abs(_value);
+        string[] suffixes = { "", "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No", "Dc", "Ud", "Dd", "Td", "Qad", "Qid", "Sxd", "Spd", "Ocd", "Nod", "Vg", "Uvg", "XXX" };
+
+        int index = 0;
+        while (absNumber >= 1000 && index < suffixes.Length - 1)
         {
-            pointsValue -= autoIncrementCost;
-            pointsValueText.text = $"Button Points: {pointsValue}";
+            absNumber /= 1000;
+            index++;
+        }
 
-            ++autoIncrementLevel;
-            autoIncrementLevelText.text = $"Auto-Click Level: {autoIncrementLevel}";
 
-            autoIncrementValue += (autoIncrementCost / 2);
-            autoIncrementValueText.text = $"Auto-Click per Second: {autoIncrementValue}";
-
-            autoIncrementCost *= Mathf.RoundToInt(1.5f);
-            autoIncrementCostText.text = $"Auto-Upgrade Cost: {autoIncrementCost}";
+        if(index == 0)
+        {
+            return $"{absNumber}{suffixes[index]}";
         }
         else
         {
-            Debug.Log("Not enough money for auto increment.");
-        }
-
-        if(autoIncrementLevel >= autoIncrementSpeedRequiredValue)
-        {
-            autoIncrementSpeedPanel.SetActive(true);
-        }
-    }
-
-    public void OnUpgradeAutoIncrementSpeed()
-    {
-        if(autoIncrementLevel < autoIncrementSpeedRequiredValue)
-        {
-            return;
-        }
-
-        if (pointsValue >= autoIncrementSpeedCost)
-        {
-            if(autoIncrementinSecond >= 0.01f)
-            {
-                pointsValue -= autoIncrementSpeedCost;
-                float _value = 1;
-                autoIncrementSpeedCost *= Mathf.RoundToInt(_value + autoIncrementinSecond);
-                autoIncrementSpeedCostText.text = $"Auto Increment Speed Cost: {autoIncrementSpeedCost}";
-
-                autoIncrementinSecond -= 0.1f;
-                if(autoIncrementinSecond <= 0.01f)
-                {
-                    autoIncrementinSecond = 0.01f;
-                    Debug.Log("You've reached the max speed!");
-                }
-
-                autoIncrementSpeedText.text = $"Auto Increment Speed: {Math.Round(autoIncrementinSecond, 2)} second/s";
-            }
-
-        }
-        else
-        {
-            Debug.Log("Not enough money for auto increment speed");
-        }
-    }
-
-    IEnumerator AutoIncrementUpdateCoroutine()
-    {
-        yield return new WaitForSeconds(autoIncrementinSecond);
-
-        if(autoIncrementLevel >= 1)
-        {
-            pointsValue += autoIncrementValue;
-            pointsValueText.text = $"Button Points: {pointsValue}";
-
-        }
-
-        StartCoroutine(AutoIncrementUpdateCoroutine());
-    }
-
-    private void SetPointText()
-    {
-        pointsValueText.text = $"Button Points: {pointsValue}";
-        pointsIncrementText.text = $"Click = {pointsIncrement}";
-        pointsIncrementCostText.text = $"Upgrade Cost: {incrementUpgradeCost}";
-        pointsIncrementLevelText.text = $"Click Me! Level: {incrementUpgradeLevel}";
-
-        autoIncrementLevelText.text = $"Auto-Click Level: {autoIncrementLevel}";
-        autoIncrementValueText.text = $"Auto-Click per Second: {autoIncrementValue}";
-        autoIncrementCostText.text = $"Auto-Upgrade Cost: {autoIncrementCost}";
-
-        autoIncrementSpeedCostText.text = $"Auto Increment Speed Cost: {autoIncrementSpeedCost}";
-        autoIncrementSpeedText.text = $"Auto Increment Speed: {Math.Round(autoIncrementinSecond, 2)} second/s";
+            return $"{absNumber:F2}{suffixes[index]}";
+        }   
     }
 
 }
---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] AudioSource audio;
+
+    UIDocument _uiDocument;
+    VisualElement _mainDivider;
+
+    VisualElement _dividerMain;
+    Label _textClickPoints;
+    Button _buttonClicker;
+    Label _textClickerGenerate;
+    Label _textAutoClickSecond;
+
+    VisualElement _dividerUpgrade;
+    Label _textUpgradeLevel;
+    Button _buttonUpgrade;
+    Label _textUpgradeCost;
+
+    VisualElement _dividerAutoClick;
+    Label _textAutoClickLevel;
+    Button _buttonAutoClick;
+    Label _textAutoClickCost;
+
+    private void Start()
+    {
+        InitializeComponents();
+        Clicky();
+        StartCoroutine(AutoClickCoroutine());
+    }
+
+    IEnumerator AutoClickCoroutine()
+    {
+        while (true)
+        {
+            if (GameManager.manager.isAutoClickEnable)
+            {
+                yield return new WaitForSeconds(1f);
+                GameManager.manager._clickCounter += GameManager.manager._clickAutoPoints;
+                _textClickPoints.text = $"Click Points: {GameManager.manager.FormattedNumber(GameManager.manager._clickCounter)}";
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+
+
+    void Clicky()
+    {
+        _buttonClicker.clicked += ButtonClicker;
+        _buttonUpgrade.clicked += ButtonUpgrade;
+        _buttonAutoClick.clicked += ButtonAutoClick;
+    }
+
+    void ButtonClicker()
+    {
+        audio.Play();
+        _dividerUpgrade.style.display = DisplayStyle.Flex;
+
+        GameManager.manager._clickCounter += GameManager.manager._clickPoints;
+        _textClickPoints.text = $"Click Points: {GameManager.manager.FormattedNumber(GameManager.manager._clickCounter)}";
+    }
+
+    void ButtonUpgrade()
+    {
+        if (GameManager.manager._clickCounter >= GameManager.manager._upgradeCost)
+        {
+            audio.Play();
+            GameManager.manager.SetNextUpgrade();
+
+            _textClickPoints.text = $"Click Points: {GameManager.manager.FormattedNumber(GameManager.manager._clickCounter)}";
+            _textClickerGenerate.text = $"Points per Click: {GameManager.manager.FormattedNumber(GameManager.manager._clickPoints)}";
+
+            _textUpgradeLevel.text = $"Upgrade Level: {GameManager.manager.FormattedNumber(GameManager.manager._upgradeLevel)}";
+            _textUpgradeCost.text = $"Upgrade Cost: {GameManager.manager.FormattedNumber(GameManager.manager._upgradeCost)}";
+            
+            if(GameManager.manager._upgradeLevel >= 10)
+            {
+                _dividerAutoClick.style.display = DisplayStyle.Flex;
+                _textAutoClickSecond.style.display = DisplayStyle.Flex;
+            }
+        }
+        else
+        {
+            Debug.Log("Not Enough Points!");
+        }
+    }
+
+    void ButtonAutoClick()
+    {
+        if (GameManager.manager._clickCounter >= GameManager.manager._clickAutoCost)
+        {
+            audio.Play();
+            GameManager.manager.SetNextAutoClick();
+
+            _textClickPoints.text = $"Click Points: {GameManager.manager.FormattedNumber(GameManager.manager._clickCounter)}";
+
+            _textAutoClickLevel.text = $"Auto Click Level: {GameManager.manager.FormattedNumber(GameManager.manager._clickAutoLevel)}";
+            _textAutoClickCost.text = $"Auto Click Cost: {GameManager.manager.FormattedNumber(GameManager.manager._clickAutoCost)}";
+            _textAutoClickSecond.text = $"Auto Click per Second: {GameManager.manager.FormattedNumber(GameManager.manager._clickAutoPoints)}";
+        }
+        else
+        {
+            Debug.Log("Not Enough Points!");
+        }
+    }
+
+    void InitializeComponents()
+    {
+        _uiDocument = GetComponent<UIDocument>();
+        _mainDivider = _uiDocument.rootVisualElement;
+
+        _dividerMain = _mainDivider.Q<VisualElement>("dividermain");
+        _textClickPoints = _dividerMain.Q<Label>("textclickpoints");
+        _buttonClicker = _dividerMain.Q<Button>("buttonclicker");
+        _textClickerGenerate = _dividerMain.Q<Label>("textclickgenerate");
+        _textAutoClickSecond = _dividerMain.Q<Label>("textautoclicksecond");
+
+        _dividerUpgrade = _mainDivider.Q<VisualElement>("dividerupgrade");
+        _textUpgradeLevel = _dividerUpgrade.Q<Label>("textupgradelevel");
+        _buttonUpgrade = _dividerUpgrade.Q<Button>("buttonupgrade");
+        _textUpgradeCost = _dividerUpgrade.Q<Label>("textupgradecost");
+
+        _dividerAutoClick = _mainDivider.Q<VisualElement>("dividerautoclick");
+        _textAutoClickLevel = _dividerAutoClick.Q<Label>("textautoclicklevel");
+        _buttonAutoClick = _dividerAutoClick.Q<Button>("buttonautoclick");
+        _textAutoClickCost = _dividerAutoClick.Q<Label>("textautoclickcost");
+
+
+        _textClickPoints.text = $"Click Points: {GameManager.manager._clickCounter}";
+        _textClickerGenerate.text = $"Points per Click: {GameManager.manager._clickPoints}";
+        _textAutoClickSecond.text = $"Auto Click per Second: {GameManager.manager._clickAutoPoints}";
+        _textAutoClickSecond.style.display = DisplayStyle.None;
+
+        _dividerUpgrade.style.display = DisplayStyle.None;
+        _textUpgradeLevel.text = $"Upgrade Level: {GameManager.manager._upgradeLevel}";
+        _textUpgradeCost.text = $"Upgrade Cost: {GameManager.manager._upgradeCost}";
+
+        _dividerAutoClick.style.display = DisplayStyle.None;
+        _textAutoClickLevel.text = $"Auto Click Level: {GameManager.manager._clickAutoLevel}";
+        _textAutoClickCost.text = $"Auto Click Cost: {GameManager.manager._clickAutoCost}";
+    }
+}
